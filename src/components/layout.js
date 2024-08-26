1760f284c7fa104e263f7342a6f3fea57eb06856
beiatrix <bpedrasa@gmail.com>
2024-08-26 18:43:45 -0500

    feat: graphql, useSiteMetadata hook

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,23 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { useSiteMetadata } from "../hooks/use-site-metadata"
 import { 
   container,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText
+  navLinkText,
+  siteTitle
 } from "./layout.module.css"
 
 // import Header from "./header"
 
 const Layout = ({ pageTitle, children }) => {
+  const { title } = useSiteMetadata()
+
   return (
     <div className={container}>
+      <header className={siteTitle}>{title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
