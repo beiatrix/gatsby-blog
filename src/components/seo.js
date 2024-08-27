360606623d7898688c6e6e1312b8374d8e158a1a
beiatrix <bpedrasa@gmail.com>
2024-08-27 18:42:55 -0500

    feat: blog post hero image

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import { useSiteMetadata } from '../hooks/use-site-metadata'
+import * as React from "react"
+import { useSiteMetadata } from "../hooks/use-site-metadata"
 
 const Seo = ({ title: pageTitle }) => {
   const { title: siteTitle } = useSiteMetadata()
--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,12 +1,29 @@
 import * as React from "react"
 import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 
 const BlogPost = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>{data.mdx.frontmatter.date}</p>
+      <small>Posted: {data.mdx.frontmatter.date}</small>
+      <br />
+      <br />
+      <GatsbyImage 
+        image={image}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />
+      <br />
+      <br />
+      <small>
+        Photo Credit •&nbsp;
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </small>
       {children}
     </Layout>
   )
@@ -14,10 +31,18 @@ const BlogPost = ({ data, children }) => {
 
 export const query = graphql`
   query ($id: String) {
-    mdx(id: {eq: $id}) {
+    mdx(id: { eq: $id }) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM D, yyyy")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
